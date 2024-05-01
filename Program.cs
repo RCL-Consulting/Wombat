8219b5bc6084ae1cfb9992755f2229629dde1279
Renier Cloete <renier@prokon.com>
2024-05-01 15:03:37 +0200

    Pre-change commit

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<WombatUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddTransient<IEmailSender>(s => new EmailSender("localhost", 25, "no-reply@wombat.com"));
 
