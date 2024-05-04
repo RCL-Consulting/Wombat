6cde41655bbdce65442134ba44abf873f227a0b9
Renier Cloete <renier@prokon.com>
2024-05-04 15:54:45 +0200

    Retry email

--- Wombat.Web/Services/EmailSender.cs
+++ Wombat.Web/Services/EmailSender.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
 using System.Net.Mail;
+using Azure;
+using Azure.Communication.Email;
 
 namespace Wombat.Services
 {
@@ -9,31 +12,58 @@ namespace Wombat.Services
         private readonly int port;
         private readonly string fromEmailAddress;
 
-        public EmailSender(string smtpServer, int port, string fromEmailAddress)
+        public EmailSender( string smtpServer, int port, string fromEmailAddress)
         {
             this.smtpServer=smtpServer;
             this.port=port;
             this.fromEmailAddress=fromEmailAddress;
         }
 
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string recipient, string subject, string htmlContent)
         {
-            var message = new MailMessage
-            {
-                From = new MailAddress(fromEmailAddress),
-                Subject = subject,
-                Body = htmlMessage,
-                IsBodyHtml = true
-            };
+            //var message = new MailMessage
+            //{
+            //    From = new MailAddress(fromEmailAddress),
+            //    Subject = subject,
+            //    Body = htmlMessage,
+            //    IsBodyHtml = true
+            //};
 
-            message.To.Add(new MailAddress(email));
+            //message.To.Add(new MailAddress(email));
 
-            using (var client = new SmtpClient(smtpServer, port))
+            //using (var client = new SmtpClient(smtpServer, port))
+            //{
+            //    client.Send(message);
+            //}
+
+            string connectionString = Environment.GetEnvironmentVariable("COMMUNICATION_SERVICES_CONNECTION_STRING");
+            EmailClient emailClient = new EmailClient(connectionString);
+
+            var sender = "donotreply@wombatweb20240504122946.azurewebsites.net";
+
+            try
             {
-                client.Send(message);
+                Console.WriteLine("Sending email...");
+                EmailSendOperation emailSendOperation = await emailClient.SendAsync(
+                    Azure.WaitUntil.Completed,
+                    sender,
+                    recipient,
+                    subject,
+                    htmlContent);
+                EmailSendResult statusMonitor = emailSendOperation.Value;
+
+                Console.WriteLine($"Email Sent. Status = {emailSendOperation.Value.Status}");
+
+                /// Get the OperationId so that it can be used for tracking the message for troubleshooting
+                string operationId = emailSendOperation.Id;
+                Console.WriteLine($"Email operation id = {operationId}");
+            }
+            catch (RequestFailedException ex)
+            {
+                /// OperationID is contained in the exception message and can be used for troubleshooting purposes
+                Console.WriteLine($"Email send operation failed with error code: {ex.ErrorCode}, message: {ex.Message}");
             }
 
-            return Task.CompletedTask;
         }
     }
 }
